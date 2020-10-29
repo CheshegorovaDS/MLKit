00e17b8bd4a5db824400ca3922e658a74c75faff
dcheshegorova <peschink@mail.ru>
2020-10-29 18:26:26 +0600

    Add library in project.

--- app/src/main/java/com/novikova/mlkit/MainActivity.kt
+++ app/src/main/java/com/novikova/mlkit/MainActivity.kt
@@ -1,11 +1,58 @@
 package com.novikova.mlkit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.AfterPermissionGranted
+import pub.devrel.easypermissions.EasyPermissions
+
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val CAMERA_PERMISSION_REQUEST_CODE = 123
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.setOnClickListener {
+            if (hasCameraPermission()) {
+                scanBarcode()
+            } else {
+                getPermission()
+            }
+        }
+    }
+
+    private fun getPermission() {
+        EasyPermissions.requestPermissions(this, "Необходим доступ к камере",
+            CAMERA_PERMISSION_REQUEST_CODE, Manifest.permission.CAMERA
+        )
+    }
+
+    private fun hasCameraPermission(): Boolean {
+        return EasyPermissions.hasPermissions(this, Manifest.permission.CAMERA)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @AfterPermissionGranted(CAMERA_PERMISSION_REQUEST_CODE)
+    private fun scanBarcode() {
+        startActivity(Intent(this, ScannerBarcodeActivity::class.java))
     }
 }
